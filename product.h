a9a0d5bb932aa6f5e1b8b6b139698900545725ec
HwangSoobin <21900813@handong.edu>
2020-04-14 20:13:12 +0900

    selectmenu-> selectMenu

--- product.h
+++ product.h
@@ -9,7 +9,7 @@ typedef struct Product{
     int sell_c; // 판매개수
 }Product;
 
-int selectmenu(); // 메뉴선택
+int selectMenu(); // 메뉴선택
 int deleteProduct(Product*pp); //제품삭제
 void readProduct(Product p); //제품 읽기
 int addProduct(Product * pp); //제품 추가
