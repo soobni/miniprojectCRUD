51646cc24653138bf0e15b81adb936b00c2a12f8
HwangSoobin <21900813@handong.edu>
2020-04-14 19:01:59 +0900

    제품 찾는 함수 추가

--- manager.c
+++ manager.c
@@ -50,3 +50,95 @@ int loadProduct(Product *p){
     printf("=> 로딩 성공!\n");
     return n;
 }
+
+void searchProduct(Product *pp, int count ){
+    int selectnum=0;
+	int c=0;
+	printf("무엇으로 검색하시겠습니까? 1:이름 2:가격대 3:별점 4:판매량");
+	scanf("%d",&selectnum);
+	if(selectnum>4||selectnum<1){
+	printf("잘못입력하였습니다.\n");
+	}
+        if(selectnum==1){
+                char name[30];
+                printf("검색할 이름을 입력하십시오.");
+                getchar();
+                scanf("%[^\n]",name);
+                getchar();
+                for(int i=0;i<count;i++){
+                        if(pp[i].price!=-1){
+                                if(strstr(pp[i].name,name)){
+                                        printf("%2d\t",i+1);
+                                        readProduct(pp[i]);
+                                        c++;
+                                }
+                        }
+                }
+                if(c==0) printf("검색된 제품이 없습니다.\n");
+        }
+        else if(selectnum==2){
+                int search=0;
+                int ud=0;
+                printf("가격대 검색 1:이상검색 2:이하검색 ");
+                scanf("%d",&ud);
+                if(ud==1){
+                        printf("얼마 이상의 상품의 가격을 검색하시겠습니까? ");
+                        scanf("%d",&search);
+                        for(int i=0;i<count;i++){
+                        if(pp[i].price!=-1){
+                                if(pp[i].price>=search){
+                                        printf("%2d\t",i+1);
+                                        readProducts(pp[i]);
+                                        c++;
+                                }
+                        }
+                }
+                
+                }
+                else{
+                        printf("얼마 이하의 상품의 가격을 검색하시겠습니까? ");
+                        scanf("%d",&search);
+                        for(int i=0;i<count;i++){
+                        if(pp[i].price!=-1){
+                                if(pp[i].price<=search){
+                                        printf("%2d\t",i+1);
+                                        readProducts(pp[i]);
+                                        c++;
+                                }
+                        }
+                        }
+                }
+                if(c==0)printf("검색된 제품이 없습니다.\n");
+
+        }
+        else if(selectnum==3){
+                //별점
+                int search=0;
+                printf("별점 몇점 이상의 상품을 원하냐요? ");
+                scanf("%d",&search);
+                for(int i=0;i<count;i++){
+                        if(pp[i].t_star>=search){
+                                printf("%2d\t",i+1);
+                                readProducts(pp[i]);
+                                c++;
+                        }
+                }
+                if (c==0) printf("제품이 없습니다.\n");
+        }
+        else {
+                //판매개수
+                int search=0;
+                printf("얼마 이상 팔린 제품을 원하시냐요? ");
+                scanf("%d",&search);
+                for(int i=0;i<count;i++){
+                        if(pp[i].sell_c>=search){
+                                printf("%2d\t",i+1);
+                                readProducts(pp[i]);
+                                c++;
+                        }
+                }
+                if (c==0) printf("제품이 없습니다.\n");
+
+        }
+}
+
