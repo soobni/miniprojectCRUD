88fd2fa5c230459bf03457de31be90e50893e0c2
HwangSoobin <21900813@handong.edu>
2020-04-14 18:31:16 +0900

    manager.h 수정 for 다중데이터용

--- manager.h
+++ manager.h
@@ -5,3 +5,9 @@ int selectdata(Product p[],int n); //인댁스 구하기₩
 
 void readProducts(Product p);
 void listProduct(Product p[],int n);
+
+int buyProduct(Product *pp,int index); //제품 사기;
+int loadProduct(Product *pp); // 불러오기
+void saveProduct(Product p[],int count); //제품 저장
+void searchProduct(Product *pp, int count);
+void searchindex(Product *pp);
